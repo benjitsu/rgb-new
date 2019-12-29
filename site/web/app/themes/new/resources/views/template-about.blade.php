69400f672b5e884e07a577202237ddecdfbbfa59
Ben <benjitsu@users.noreply.github.com>
2019-12-28 19:15:36 -0600

    updates

--- site/web/app/themes/new/resources/views/template-about.blade.php
+++ site/web/app/themes/new/resources/views/template-about.blade.php
@@ -21,7 +21,17 @@
   <div class="aboutSide"></div>
 </div>
 </div>
+</div>
+<div class="container-fluid mb7 bg-light pt7 pb7">
+  <div class="container">
+<div class="row">
+<div class="col-lg-10 mx-auto">
+<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed//LXd-E8Yy-8M' frameborder='0' allowfullscreen></iframe></div>
+</div>
+</div>
+</div>
+</div>
   @endwhile
   @include('partials.capture')
-</div>
+
 @endsection
