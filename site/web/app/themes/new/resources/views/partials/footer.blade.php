619a764348337fedfd696e5bb2abeefc8d720be1
Ben <benjitsu@users.noreply.github.com>
2021-10-12 21:18:03 -0700

    youtube added to footer

--- site/web/app/themes/new/resources/views/partials/footer.blade.php
+++ site/web/app/themes/new/resources/views/partials/footer.blade.php
@@ -3,6 +3,7 @@
     <div class="row justify-content-center">
     <a href="https://www.instagram.com/ralphgracieberkeley" target="_blank"><i class="fab fa-instagram"></i></a>
     <a href="https://www.facebook.com/ralphgracieberkeley/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+    <a href="https://www.youtube.com/channel/UCG9QiexcRH0hDuo2X2AmOxQ" target="_blank"><i class="fab fa-youtube"></i></a>
     </div>
     <div class="row justify-content-center">© 2021 Ralph Gracie Berkeley Brazilian Jiu-Jitsu</div>
   </div>
