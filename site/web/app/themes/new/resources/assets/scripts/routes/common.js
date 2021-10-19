c274d555bec1207438ee1d9e65df491fb6c552ed
Ben <benjitsu@users.noreply.github.com>
2021-10-18 17:55:49 -0700

    homepage hero images and action updated

--- site/web/app/themes/new/resources/assets/scripts/routes/common.js
+++ site/web/app/themes/new/resources/assets/scripts/routes/common.js
@@ -6,7 +6,17 @@ export default {
       $('.navbar-collapse').toggleClass('show');
     });
 
+const hero = document.querySelector('#homeHero .jumbotron')
 
+function randNum(min, max) { 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+if(hero){
+const num = randNum(1,5);
+const numString = 'hero-' + num.toString();
+hero.classList.add(numString);
+}
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
